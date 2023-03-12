da6d5c1dacfa2d0109c312f2c865d53fbfa247ce
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-11 19:11:32 -0600

    strings concatenar

---
+++ strings2.py
@@ -0,0 +1,22 @@
+concatenar = "Arriba " + "el Atlas."
+
+print(concatenar)
+
+string1 = "Husky"
+string2 = "Siberiano"
+mensaje = string1 + ' ' + string2
+print(mensaje)
+
+palabra = "Husky "
+print(palabra + "Siberiano")
+
+nombre = "Miguel"
+apellido1 = "Ramirez"
+apellido2 = "Santana"
+
+nombreCompleto = nombre + " " + apellido1 + " " + apellido2
+
+print(nombreCompleto)
+
+numero = "20" + "23"
+print(numero)
