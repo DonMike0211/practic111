2e85c4a975f9f6e60793b6151e1cba4e4c1462b2
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-11 20:39:53 -0600

    suma, resta, multiplicación y división

---
+++ operadores1.py
@@ -0,0 +1,10 @@
+suma = 20 + 23 + 44
+print(suma)
+resta = 20 - 23 - 84
+print(resta)
+multiplicacion = 10 * 87
+print(multiplicacion)
+division = 5000 / 230 / 2
+print(division)
+operacion = 10 / 5 + 15 - 17
+print(operacion)
