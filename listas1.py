2628ad721bcb451b51ec4e36221312ababd2cb9e
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-11 20:56:17 -0600

    crear listas y utilizarlas

---
+++ listas1.py
@@ -0,0 +1,8 @@
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(colores)
+
+mensaje = 'En la posicion 3 esta el amarillo' + ', el rojo "0" y el rosa "7"'
+print(mensaje)
+
+colores = ["tres", "dos", "cinco", "cuatro", "uno"]
+print(colores)
