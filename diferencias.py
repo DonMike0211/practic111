1ac94cff8a14629e4694a175d7ff72f5c9278c91
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-11 22:12:23 -0600

    crear y manejar tuplas

---
+++ diferencias.py
@@ -0,0 +1,7 @@
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+
+print(colores[1])
+
+numeros = (10, 1, 5, 11)
+
+print(numeros[0] + numeros[2] + numeros[3] - numeros[1])
