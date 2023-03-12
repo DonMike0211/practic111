e8157255a0f815b788ef5672c0a7277d4f2b2a05
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-11 19:23:17 -0600

    strings upper y lower

---
+++ strings3.py
@@ -0,0 +1,9 @@
+nombre = "enrique barros fernández".upper()
+print(nombre)
+
+frase = "Esta Es Una Frase Para Ser Formateada.".lower()
+print(frase)
+
+
+frase = "Esta Es Una Frase Para Ser Formateada.".upper()
+print(frase)
