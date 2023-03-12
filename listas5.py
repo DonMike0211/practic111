e331073206c7c3b478851db524da1ea86c4dce69
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-11 21:39:23 -0600

    eliminar con pop

---
+++ listas5.py
@@ -0,0 +1,6 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+valor1 = colores.pop(1)
+valor2 = colores.pop(7)
+
+print('Nueva lista:', valor1 , 'y', valor2 )
