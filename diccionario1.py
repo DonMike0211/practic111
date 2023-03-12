91bc9f80ddfc7f5f9d77ebcaea5418f6cdc4cd17
DonMike0211 <125336692+DonMike0211@users.noreply.github.com>
2023-03-12 14:35:40 -0600

    diccionarios en python

---
+++ diccionario1.py
@@ -0,0 +1,16 @@
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+consulta = print('El modelo ' + teclado2['Modelo'] + ' cuesta ' + teclado2['Precio'] + '$')
+print(consulta)
+
+
